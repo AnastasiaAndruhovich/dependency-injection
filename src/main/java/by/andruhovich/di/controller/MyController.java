dddb5830244a9b57c2567ea594bce4515b5826df
Anastasia-Andruhovich <anastasia.andruhovich@ibm.com>
2021-07-19 23:16:50 +0300

    Add Primary autowiring

--- src/main/java/by/andruhovich/di/controller/MyController.java
+++ src/main/java/by/andruhovich/di/controller/MyController.java
@@ -1,13 +1,17 @@
 package by.andruhovich.di.controller;
 
+import by.andruhovich.di.service.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String sayHello() {
-        System.out.println("Hello World!!!");
-
-        return "Hi Folks!";
+        return greetingService.sayGreeting();
     }
 }
