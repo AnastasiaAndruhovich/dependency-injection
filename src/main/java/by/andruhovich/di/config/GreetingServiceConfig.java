7d917283ccf4375f6f3ca7635e7bee11ea3401a2
Anastasia-Andruhovich <anastasia.andruhovich@ibm.com>
2021-09-15 08:21:07 +0300

    Replace primary annotations with the Configuration class

--- src/main/java/by/andruhovich/di/config/GreetingServiceConfig.java
+++ src/main/java/by/andruhovich/di/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package by.andruhovich.di.config;
 
-import by.andruhovich.di.service.ConstructorGreetingService;
-import by.andruhovich.di.service.PropertyGreetingService;
-import by.andruhovich.di.service.SetterGreetingService;
+import by.andruhovich.di.service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
